a56498c466de25f827073109042b7e9bc0c6d86f
kailee1st <145636527+kailee1st@users.noreply.github.com>
2026-04-23 19:50:24 +0900

    fix: export app from firebase.js for Storage

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -14,6 +14,6 @@ const firebaseConfig = {
   measurementId:     "G-MGJSJE50B5",
 };
 
-const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+export const app  = getApps().length ? getApp() : initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db   = getFirestore(app);
